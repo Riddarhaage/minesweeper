17910a86141666b74da37aa6988663436d1117b1
Ruben Riddarhaage <ruben_riddarhaage@hotmail.com>
2022-12-20 21:05:11 +0100

    half done function to open adj cells

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -19,7 +19,7 @@ def displayBoard():
                 print(f"\033[4m{boardDisplay[row][col]}\033[0m", end="\033[4m" + " |" + "\033[0m")
         print("")
 
-def checkAdjCells(row, col):
+def checkAdjCells(row,col):
     adjMines = 0
     r = row - 1
     while r <= row+1:
@@ -27,10 +27,26 @@ def checkAdjCells(row, col):
             c = col - 1
             while c <= col + 1:
                 if c>=0 and c<5:
-                    adjMines += board[r][c]
+                    adjMines += board[r][c] # 1 added to adjMines since mines = 1 on the board
                 c += 1
         r += 1
     return adjMines
+
+def openAdjEmpyCell(row, col):
+    r = row - 1
+    while r <= row+len(board):
+        if r>=0 and r <len(board):
+            c = col - 1
+            while c <= col + len(board):
+                if c>=0 and c<len(board):
+                    if board[r][c] == 0:
+                       _ = boardDisplay[r][c] = 0
+                    elif board[r][c] != 0:
+                        test = checkAdjCells(row,col)
+                        return test
+                c += 1
+        r += 1
+    return _
 
 txtUnderline = "\033["
 endUnderline = "\033[0m"
@@ -67,7 +83,7 @@ boardDisplay = [
 
 numOfMines = int(input("Amount of mines: "))
 if numOfMines == 0:
-    numOfMines = 5
+    numOfMines = 1
     print("not enought mines to make things interesting!")
     print(f"{numOfMines} mines added to the board")
 if numOfMines > 75:
@@ -99,6 +115,7 @@ while picks < (100-numOfMines):
         print("you lost!")
         displayNakedBoard()
     else:
+        boardDisplay[row][col] = openAdjEmpyCell(row, col)
         boardDisplay[row][col] = checkAdjCells(row,col)
         displayBoard()
     
