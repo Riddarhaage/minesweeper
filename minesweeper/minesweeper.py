0c1e7e10862d785d565645201e06a9267d5c1563
Ruben Riddarhaage <ruben_riddarhaage@hotmail.com>
2022-12-26 11:22:31 +0100

    wrong input handling with try/except added

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -2,10 +2,20 @@
 import random
 
 def displayNakedBoard():
-    for row in range(0, len(board)):
-        for col in range(0, len(board)):
-            #ANSI code to add underlines for a prettier grid
-            print(f"\033[4m{board[row][col]}\033[0m", end="\033[4m" + " |" + "\033[0m")
+    value_symbol_map = {
+        1: "*",
+        0: " ",
+        "F": "F"
+    }
+    print("  ", end="") 
+    for col in range(0, 10):  # Print the column index
+        print(f"\033[4m {col} \033[0m", end="")
+    print("") 
+    for row in range(0, 10):  # Print the row index
+        print(f"{row}|", end="")
+        for col in range(0, 10):
+            symbol = value_symbol_map.get(board[row][col], " ")
+            print(f"\033[4m{symbol}\033[0m", end="\033[4m" + " |" + "\033[0m")
         print("")
 
 def displayBoard():
@@ -16,7 +26,7 @@ def displayBoard():
     for row in range(0, 10):  # Print the row index
         print(f"{row}|", end="")
         for col in range(0, 10):
-            if boardDisplay[row][col] == hiddenCell:
+            if boardDisplay[row][col] == HIDDENCELL:
                 print("\033[4m" + " " + "\033[0m", end="\033[4m" + " |" + "\033[0m")
             else:
                 print(f"\033[4m{boardDisplay[row][col]}\033[0m", end="\033[4m" + " |" + "\033[0m")
@@ -46,26 +56,26 @@ def openAdjEmpyCell(row, col):
             row = offset[0] + cell[0]
             col = offset[1] + cell[1]
             if((row>=0 and row<=9) and (col>=0 and col<=9)):
-                if((boardDisplay[row][col]==hiddenCell) and (board[row][col]==empty)):
+                if((boardDisplay[row][col]==HIDDENCELL) and (board[row][col]==EMPTY)):
                     boardDisplay[row][col] = checkAdjCells(row,col)
 
-                    if checkAdjCells(row,col) == empty and (row,col) not in cells:
+                    if checkAdjCells(row,col) == EMPTY and (row,col) not in cells:
                         cells.append((row,col))
                     else:
                         boardDisplay[row][col] = checkAdjCells(row,col)
 
 def setFlag(row, col):
     if boardDisplay[row][col] == "F":
-        boardDisplay[row][col] = hiddenCell
+        boardDisplay[row][col] = HIDDENCELL
         
     else:
         boardDisplay[row][col] = "F"
 
     
 
-bomb = 1
-hiddenCell = -1
-empty = 0
+MINE = 1
+HIDDENCELL = -1
+EMPTY = 0
         #row and cols 10x10 grid
         #not visible to user
 board = [
@@ -110,17 +120,17 @@ while mines < numOfMines:
     bombRow = random.randint(0, len(board)-1)
     bombCol = random.randint(0, len(board)-1)
     if board[bombRow][bombCol] == 0:
-        board[bombRow][bombCol] = bomb
+        board[bombRow][bombCol] = MINE
     mines += 1
 
 
 displayNakedBoard()
+print("")
 displayBoard()
 
-picks = 0
-
-while picks < (100-numOfMines):
-    flag = input("place/remove flag?(y/n): ")
+complete = any(-1 in x for x in boardDisplay)
+while complete:
+    flag = input("place/remove flag?(y/n): ").lower()
     if flag == 'y':
         cell = input("pick a cell:")
         splitCell = cell.split(',')
@@ -128,19 +138,28 @@ while picks < (100-numOfMines):
         col = int(splitCell[1])
         setFlag(row,col)
         displayBoard()
+        complete = any(-1 in x for x in boardDisplay)
     elif flag == 'n':
-        cell = input("pick a cell:")
-        splitCell = cell.split(',')
-        row = int(splitCell[0])
-        col = int(splitCell[1])
-        if boardDisplay[row][col] == 'F':
-            print("Can't open a flagged cell!")
-        elif board[row][col] == bomb:
-            print("you lost!")
-            displayNakedBoard()
-            break
-        else:
-            boardDisplay[row][col] = openAdjEmpyCell(row, col)
-            boardDisplay[row][col] = checkAdjCells(row, col)
-            displayBoard()
-    
+        try:
+            cell = input("pick a cell:")
+            splitCell = cell.split(',')
+            row = int(splitCell[0])
+            col = int(splitCell[1])
+        except:
+            print("\u001b[31mERROR:\u001b[37m.Try to enter a valid row & column seperated by a comma")
+        
+        try:
+            if boardDisplay[row][col] == 'F':
+                print("Can't open a flagged cell!")
+            elif board[row][col] == MINE:
+                print("you lost!")
+                displayNakedBoard()
+                break
+            else:
+                boardDisplay[row][col] = openAdjEmpyCell(row, col)
+                boardDisplay[row][col] = checkAdjCells(row, col)
+                displayBoard()
+                complete = any(-1 in x for x in boardDisplay)
+        except:
+            pass
+print("Congratulations! you won!")
